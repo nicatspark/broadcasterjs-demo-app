34a87c221ef18413f9eeb8957f8641a8c355ff00
Nicolas Hervy <nicolas.hervy@skf.com>
2022-02-05 11:02:02 +0100

    Advanced use text.

--- src/pages/PageDebug.tsx
+++ src/pages/PageDebug.tsx
@@ -24,8 +24,8 @@ export const PageDebug = () => {
         </li>
         <li>
           <summary className='limited'>
-            Activate a <mark>global</mark> debugmode that outputs all events to
-            the console log.
+            Activate a <mark>global</mark> debugmode that outputs all emit
+            events as well as subscriptions when they occur to the console log.
             <details>
               Add <mark>?debug=broadcasterjs</mark> to your url and open the
               devtools console.
@@ -35,7 +35,7 @@ export const PageDebug = () => {
         <li>
           <summary className='limited'>
             Activate debugmode <mark>localy</mark> that outputs a specific
-            subscription/emit to the console log.
+            subscription/emit event to the console log.
             <details>See the example below.</details>
           </summary>
         </li>
@@ -50,6 +50,37 @@ export const PageDebug = () => {
           </code>
         </pre>
       </Block>
+      <h2 className='limited'>Advanced use</h2>
+      <h3 className='limited'>Circumvent the doublette guard</h3>
+      <p className='limited'>
+        BroadcasterJS was optimised to run in a SPA like situation where it can
+        be a tall task to keep track of renders where subscription are set.
+        Since custom events can be set multiple times BroadcasterJS by default
+        does not allow more than one identical flag + function combination to be
+        set.
+      </p>
+      <p className='limited'>
+        It is easy to circumevent this by sligthly changing the function to
+        include a comment with a number /* 1 */ etc to ensure that the function
+        is unique. You can also disable the guard by including a settings object
+        as a third value in the subscriber array. The subscriber argument array
+        would look something like{' '}
+        {`['MY-EXAMPLE-FLAG', myFunctionToRunOnEvent, {allowDoublettesSubscribers:true}]`}
+      </p>
+      <p className='limited'>
+        The broadcast subscriber function also returns an unique id for the flag
+        + function combination if you want to handle the doublette guard
+        externally for some reason.
+      </p>
+
+      <h3 className='limited'>Invoke the global debug</h3>
+      <p className='limited'>
+        Aside from invoking it through the url with url params you can invoke it
+        throught the settings object. Debug output will then start from when the
+        settings object is set of course so the url method is preferable.
+        Example:{' '}
+        {`['MY-EXAMPLE-FLAG', myFunctionToRunOnEvent, {debugGlobal:true}]`}
+      </p>
     </Page>
   )
 }
